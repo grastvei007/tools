568e438f927f01b08c4dab54376bac1023f3873e
Stein Gunnar Grastveit <grastvei@pvv.org>
2026-02-08 11:01:26 +0100

    Set all adc 0-7 on tags

--- addaboard/src/app.cpp
+++ addaboard/src/app.cpp
@@ -25,7 +25,14 @@ App::App(int argc, char *argv[])
 	TagList::sGetInstance().setClientName("adc");
 	TagList::sGetInstance().connectToServer("localhost", 5000);
 	connect(&TagList::sGetInstance(), &TagList::connected, this, [this]() {
+		adc0Tag_ = TagList::sGetInstance().createTag("adda", "adc0", TagType::eDouble, 0.0);
 		adc1Tag_ = TagList::sGetInstance().createTag("adda", "adc1", TagType::eDouble, 0.0);
+		adc2Tag_ = TagList::sGetInstance().createTag("adda", "adc2", TagType::eDouble, 0.0);
+		adc3Tag_ = TagList::sGetInstance().createTag("adda", "adc3", TagType::eDouble, 0.0);
+		adc4Tag_ = TagList::sGetInstance().createTag("adda", "adc4", TagType::eDouble, 0.0);
+		adc5Tag_ = TagList::sGetInstance().createTag("adda", "adc5", TagType::eDouble, 0.0);
+		adc6Tag_ = TagList::sGetInstance().createTag("adda", "adc6", TagType::eDouble, 0.0);
+		adc7Tag_ = TagList::sGetInstance().createTag("adda", "adc7", TagType::eDouble, 0.0);
 	});
 
 	printf("demo\r\n");
@@ -52,12 +59,27 @@ void App::read()
 {
 	UDOUBLE ADC[8], i;
 	ADS1256_GetAll(ADC);
-	for (i = 0; i < 8; i++)
+	/*for (i = 0; i < 8; i++)
 	{
 		printf("%d %f\r\n", i, ADC[i] * 5.0 / 0x7fffff);
 	}
 	printf("\33[8A"); //Move the cursor up 8 lines
+    */
 
+	if (adc0Tag_)
+		adc0Tag_->setValue((double(ADC[0] * 5.0 / 0x7fffff)));
 	if (adc1Tag_)
 		adc1Tag_->setValue((double(ADC[1] * 5.0 / 0x7fffff)));
+	if (adc2Tag_)
+		adc2Tag_->setValue((double(ADC[2] * 5.0 / 0x7fffff)));
+	if (adc3Tag_)
+		adc3Tag_->setValue((double(ADC[3] * 5.0 / 0x7fffff)));
+	if (adc4Tag_)
+		adc4Tag_->setValue((double(ADC[4] * 5.0 / 0x7fffff)));
+	if (adc5Tag_)
+		adc5Tag_->setValue((double(ADC[5] * 5.0 / 0x7fffff)));
+	if (adc6Tag_)
+		adc6Tag_->setValue((double(ADC[6] * 5.0 / 0x7fffff)));
+	if (adc7Tag_)
+		adc7Tag_->setValue((double(ADC[7] * 5.0 / 0x7fffff)));
 }
